5364bc592c5a8a55df423fe94137620e64c360d1
jrgs <jrgarcia@iesmarenostrum.com>
2024-03-05 17:15:19 +0100

    Versión inicial con la clase comprobadorDePassword y un Form que muestra cómo utilizarla

---
+++ ComprobadorDePassword/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComprobadorDePassword
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnComprobar_click(object sender, EventArgs e)
+        {
+            comprobadorDePassword miComprobador = new comprobadorDePassword();
+            int resultado = miComprobador.test(txtPassword.Text);
+            if (resultado < 0)
+                MessageBox.Show("La contraseña no puede estar vacía");
+            if (resultado == 0)
+                MessageBox.Show("Contraseña demasiado corta");
+            if (resultado == 1)
+                MessageBox.Show("Contraseña débil");
+            if (resultado == 2)
+                MessageBox.Show("Contraseña normal");
+            if (resultado == 3)
+                MessageBox.Show("Contraseña fuerte");
+            if (resultado == 4)
+                MessageBox.Show("Contraseña muy fuerte");
+        }
+    }
+}
