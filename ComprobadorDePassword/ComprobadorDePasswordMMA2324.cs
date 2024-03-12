05406ad3a230955a0002a9d694f73b11f88259d2
MARIA MIRA ALAVES <marmirala@alu.edu.gva.es>
2024-03-12 20:09:37 +0100

    Realizada documentación de código y refactorizadas partes del método, en el foreach he cambiado la "c" por "caracter" y la variable f la he cambiado por nivelFortaleza. Añadido PDF a la carpeta del proyecto. MMA2324

--- ComprobadorDePassword/ComprobadorDePasswordMMA2324.cs
+++ ComprobadorDePassword/ComprobadorDePasswordMMA2324.cs
@@ -19,6 +19,26 @@ namespace ComprobadorDePassword
         public string ERROR_PASSWORD_VACIO = "La contraseña no puede estar vacía.";
         public string ERROR_PASSWORD_MENOR6 = "Contraseña demasiado corta";
 
+        /// <summary>
+        /// <para>Constructor que inicializa los campos que formarán parte de <see cref="password"/>.</para>
+        /// <para>Los campos son:
+        /// <list type="bullet">
+        ///     <item>
+        ///         <decription>Minusculas</decription>
+        ///     </item>
+        ///     <item>
+        ///         <description>Mayúsculas</description>
+        ///     </item>
+        ///     <item>
+        ///         <descripton>Números</descripton>
+        ///     </item>
+        ///     <item>
+        ///         <description>Longitud del password</description>
+        ///     </item>
+        /// </list>
+        /// </para>
+        /// </summary>
+        /// <remarks>Inicializa todos los campos a false.</remarks>
         public ComprobadorDePasswordMMA2324()
         {
             minusculas = mayusculas = numeros = passwordLength = false;
@@ -30,6 +50,16 @@ namespace ComprobadorDePassword
             set => password = value;
         }
 
+        /// <summary>
+        /// <para>Método que valida el string <paramref name="password"/>.</para>
+        /// <para>Valida si la contraseña tiene la longitud adecuada, y determina el nivel de fortaleza de la misma analizando su contenido (mayúsculas),
+        /// minúsculas, números y longitud mayor que 12.</para>
+        /// </summary>
+        /// <param name="password">Valor de tipo string que representa la contraseña que vamos a validar.</param>
+        /// <returns>Entero de tipo int que refleja el nivel de fortaleza que tiene <paramref name="password"/>.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">String password vacío o String con longitud menor que 6.</exception>
+        /// <remarks>El mensaje que generará la excepción ERROR_PASSWORD_VACIO será "La contraseña no puede estar vacía.</remarks>
+        /// <remarks>El mensaje que generará la excepción ERROR_PASSWORD_MENOR6 será "Contraseña demasiado corta"</remarks>
         public int TestValidacionPassword(string password)
         {
             Password = password;
@@ -52,26 +82,25 @@ namespace ComprobadorDePassword
             if (Password.Length > 12) length = true;
 
             // Recorremos la cadena buscando minúsculas, mayúsculas y números
-            //
-            foreach (char c in Password)
+            foreach (char caracter in Password)
             {
-                if (char.IsLower(c))
+                if (char.IsLower(caracter))
                 {
                     mins = true;
                 }
             }
 
-            foreach (char c in Password)
+            foreach (char caracter in Password)
             {
-                if (char.IsUpper(c))
+                if (char.IsUpper(caracter))
                 {
                     mays = true;
                 }
             }
 
-            foreach (char c in Password)
+            foreach (char caracter in Password)
             {
-                if (char.IsDigit(c))
+                if (char.IsDigit(caracter))
                 {
                     nums = true;
                 }
@@ -82,29 +111,29 @@ namespace ComprobadorDePassword
             // 3: fuerte
             // 2: normal
             // 1: débil
-            int f=0;
+            int nivelFortaleza = 0;
 
             if (mins)
             {
-                f++;
+                nivelFortaleza++;
             }
 
             if (mays)
             {
-                f++;
+                nivelFortaleza++;
             }
 
             if (nums)
             {
-                f++;
+                nivelFortaleza++;
             }
 
             if (length)
             {
-                f++;
+                nivelFortaleza++;
             }
 
-            return f;
+            return nivelFortaleza;
         }
     }
 }
