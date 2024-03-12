21bf2fb79771c0cc377acb813bd261979e81a9d3
BLANCA ARIAS <blaarisae@alu.edu.gva.es>
2024-03-12 19:25:44 +0100

    refactorizacion

--- ComprobadorDePassword/ComprobadorDePassword.cs
+++ ComprobadorDePassword/ComprobadorDePassword.cs
@@ -11,27 +11,43 @@ namespace ComprobadorDePassword
 
     public class ComprobadorDePassword
     {
-        public string password;
-
-        private bool mins;
-        private bool mays;
-        private bool nums;
+        private string password;
+        private bool minusculas;
+        private bool mayusculas;
+        private bool numeros;
         private bool length;
+        public const String ERR_CLAVE_NULA_O_VACIA = "error, clave nula o vacia";
+        public const String ERR_CLAVE_MUY_CORTA = "error, clave corta";
 
+        /// <summary>
+        /// Constructor de clase. Sirve para inicializar la clase.
+        /// </summary>
         public ComprobadorDePassword()
         {
-            mins = mays = nums = length = false;
+            Minusculas = Mayusculas = Numeros = Length = false;
         }
 
+        public bool Minusculas { get => minusculas; set => minusculas = value; }
+        public bool Mayusculas { get => mayusculas; set => mayusculas = value; }
+        public bool Numeros { get => numeros; set => numeros = value; }
+        public bool Length { get => length; set => length = value; }
+        public string Password { get => password; set => password = value; }
+
+
+        /// <summary>
+        /// Metodo por el cual pasamos una contraseña
+        /// </summary>
+        /// <param name="password">contraseña que introduce el usuario</param>
+        /// <returns>Nos devuelve -1, 0 o fortaleza de la contraseña</returns>
         public int Test(string password)
         {
-            this.password = password;
-
-            if (this.password == null || this.password.Length <= 0)
-                return -1; // Si la contraseña es nula o vacía, devolvemos un código de error
-
-            if (this.password.Length < 6)
-                return 0; // No tiene la longitud mínima, error
+            this.Password = password;
+            ///<summary>Si la contraseña es nula o vacía, devolvemos un código de error</summary>
+            if (this.Password == null || this.Password.Length <= 0)
+                throw new ArgumentOutOfRangeException(ERR_CLAVE_NULA_O_VACIA);
+            ///<summary>Si no tiene la longitud mínima, error</summary>
+            if (this.Password.Length < 6)
+                throw new ArgumentOutOfRangeException(ERR_CLAVE_MUY_CORTA);
 
 
             bool minusculas = false;
@@ -39,37 +55,32 @@ namespace ComprobadorDePassword
             bool numeros = false;
             bool length = false;
 
-            if (this.password.Length > 12) length = true;
+            ///<summary>Si longitud es mayor que 12, length será true</summary>
+            if (this.Password.Length > 12) length = true;
 
-            // Recorremos la cadena buscando minúsculas, mayúsculas y números
-            //
-            foreach (char cadena in this.password)
+            ///<summary>Recorremos la cadena para ver si existen minúsculas, mayúsculas y números</summary>
+            foreach (char cadena in this.Password)
             {
                 if (char.IsLower(cadena))
                 {
                     minusculas = true;
                 }
-            }
-            foreach (char cadena in this.password)
-            {
                 if (char.IsUpper(cadena))
                 {
                     mayusculas = true;
                 }
-            }
-            foreach (char cadena in this.password)
-            {
                 if (char.IsDigit(cadena))
                 {
                     numeros = true;
                 }
             }
-
-            // Calculamos el nivel de fortaleza
-            // 4: muy fuerte
-            // 3: fuerte
-            // 2: normal
-            // 1: débil
+            ///<summary>Calculamos el nivel de fortaleza</summary>
+            ///<returns>
+            ///<para>4: muy fuerte</para>
+            ///<para>3: fuerte</para>
+            ///<para>2: normal</para>
+            ///<para>1: debil</para>
+            /// </returns>
             int f = 0;
             if (minusculas) f++;
             if (mayusculas) f++;
