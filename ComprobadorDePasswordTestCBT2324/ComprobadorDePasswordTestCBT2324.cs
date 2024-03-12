0bac58546eeb19790a6d53e19c8656bd8642a94e
carbeltor3 <carbeltor3@alu.edu.gva.es>
2024-03-12 19:57:41 +0100

    Punto 3 creacion de las clases de prueba

---
+++ ComprobadorDePasswordTestCBT2324/ComprobadorDePasswordTestCBT2324.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ComprobadorDePassword;
+
+namespace ComprobadorDePasswordTestCBT2324
+{
+    [TestClass]
+    public class ComprobadorDePasswordTestCBT2324
+    {
+        [TestMethod]
+        public void ProbarContraseñaNula()
+        {
+            string p = "";
+
+            comprobadorDePassword miPrueba = new comprobadorDePassword ();
+
+            int resultado = miPrueba.ProbarContraseña(p);
+
+            Assert.AreEqual(resultado, -1, "contraseña nula o vacia");
+        }
+
+        [TestMethod]
+        public void ProbarContraseñaMenorDeSeis()
+        {
+            string p = "abc";
+
+            comprobadorDePassword miPrueba = new comprobadorDePassword();
+
+            int resultado = miPrueba.ProbarContraseña(p);
+
+            Assert.AreEqual(resultado, 0, "contraseña menor de 6 caracteres");
+        }
+
+        [TestMethod]
+        public void ProbarContraseñaMayorDeDoce()
+        {
+            string p = "C0ntr@s3ñ@S3gur@\"";
+
+            comprobadorDePassword miPrueba = new comprobadorDePassword();
+
+            int resultado = miPrueba.ProbarContraseña(p);
+
+            Assert.AreNotEqual(resultado,0, "contraseña nula o vacia");
+        }
+    }
+}
