1044b75c9a2b43e430bc9fe18f16e04d36a154ab
atms1dawz <angmorsen@alu.edu.gva.es>
2024-03-12 19:56:19 +0100

    Pruebas antes de refactorizar

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -7,18 +7,55 @@ namespace UnitTestProject1
     [TestClass]
     public class UnitTest1
     {
+        // PRUEBA PARA UNA PASWORDD ""
         [TestMethod]
         [DataRow(-1)]
         public void PasswordNula(int password)
         {
             // preparacion
+            string cadena = "";
             ComprobadorDePasswordATMS2324 prueba = new ComprobadorDePasswordATMS2324 ();
 
-            // funcion
-            int resultado = prueba.test("");
+            // funcion a probar
+            int resultado = prueba.test(cadena);
 
             // Assert
             Assert.AreEqual(resultado, password);
         }
+
+
+        // PRUEBA PARA UNA DE TRES VOCALES "abc"
+        [TestMethod]
+        [DataRow(0)]
+        public void PasswordTresVocales(int password)
+        {
+            // preparacion
+            string cadena = "abc";
+            ComprobadorDePasswordATMS2324 prueba = new ComprobadorDePasswordATMS2324();
+
+            // funcion a probar
+            int resultado = prueba.test(cadena);
+
+            // Assert
+            Assert.AreEqual(resultado, password);
+        }
+
+        // PRUEBA PARA UNA PASWORDD SEGURA "C0ntr@s3ñ@S3gur@"
+        [TestMethod]
+        [DataRow(4)]
+        public void PasswordSegura(int password)
+        {
+            // preparacion
+            string cadena = "C0ntr@s3ñ@S3gur@";
+            ComprobadorDePasswordATMS2324 prueba = new ComprobadorDePasswordATMS2324();
+
+            // funcion a probar
+            int resultado = prueba.test(cadena);
+
+            // Assert
+            Assert.AreEqual(resultado, password);
+        }
+
+
     }
 }
