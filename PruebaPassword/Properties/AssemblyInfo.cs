524e68a736b1e645670604707ea971c4f742a77a
BLANCA ARIAS <blaarisae@alu.edu.gva.es>
2024-03-12 18:30:19 +0100

    1PRUEBA

---
+++ PruebaPassword/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PruebaPassword")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("IES Mare Nostrum")]
+[assembly: AssemblyProduct("PruebaPassword")]
+[assembly: AssemblyCopyright("Copyright © IES Mare Nostrum 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3338d2f5-3ce3-4b75-bb5b-5db3c08c667f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PruebaPassword/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ComprobadorDePassword;
+
+
+namespace PruebaPassword
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PasswordNoValida()
+        {
+            string password = "";
+            comprobadorDePassword miPrueba = new comprobadorDePassword();
+            Assert.AreEqual(-1, miPrueba.test(password));
+        }
+    }
+}
