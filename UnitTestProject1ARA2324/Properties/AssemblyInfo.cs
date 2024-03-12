d47366547409cb72f519033477c9d7e42975d6a7
LENA CERRATO LORENTE <lencerlor@alu.edu.gva.es>
2024-03-12 20:21:41 +0100

    Pruebas Unitarias añadidas ARA2324

---
+++ UnitTestProject1ARA2324/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1ARA2324")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("IES Mare Nostrum")]
+[assembly: AssemblyProduct("UnitTestProject1ARA2324")]
+[assembly: AssemblyCopyright("Copyright © IES Mare Nostrum 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f3b878eb-20de-4622-b943-207cd2761fb1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1ARA2324/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1ARA2324
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_ContrasenyaVacia() //Debe devolver -1
+        {
+            ComprobadorDePassword comprobador = new ComprobadorDePassword();
+            int resultado = comprobador.Probar("");
+            Assert.AreEqual(-1, resultado);
+        }
+
+        [TestMethod]
+        public void Test_ContrasenyaDebil() //Debe devolver 0
+        {
+            ComprobadorDePassword comprobador = new ComprobadorDePassword();
+            int resultado = comprobador.Probar("abc");
+            Assert.AreEqual(0, resultado);
+        }
+
+        [TestMethod]
+        public void Test_ContrasenyaFuerte() //Debe devolver 4
+        {
+            ComprobadorDePassword comprobador = new ComprobadorDePassword();
+            int resultado = comprobador.Probar("C0ntr@s3ñ@S3gur@");
+            Assert.AreEqual(4, resultado);
+        }
+
+    }
+}
