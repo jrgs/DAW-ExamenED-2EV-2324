0cf99168c017922f59a617ebe6253bedce6b26ff
CNB-23-24 <cai_enamorada@hotmail.com>
2024-03-12 19:42:22 +0100

    pregunta 2

---
+++ CNB2324/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CNB2324")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("IES Mare Nostrum")]
+[assembly: AssemblyProduct("CNB2324")]
+[assembly: AssemblyCopyright("Copyright © IES Mare Nostrum 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2dcdd63d-8c05-4c47-a22e-4019aad84fb7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CNB2324/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ComprobadorDePassword;
+
+namespace CNB2324
+{
+    public class PruebaPassword2324
+    {
+        [TestMethod]
+        public void validarContrasenya1()
+        {
+            int PasswordEsperado = -1;
+            string PasswordIntroducido = "";
+
+            comprobadorDePasswordCNB2324 contrasenya = new comprobadorDePasswordCNB2324();
+
+            int ResultadoEsperado = contrasenya.Test(PasswordIntroducido);
+
+            Assert.AreEqual(PasswordEsperado, ResultadoEsperado);
+        }
+
+        [TestMethod]
+
+        public void validarContrasenya2()
+        {
+            int PasswordEsperado = 0;
+            string PasswordIntroducido = "abc";
+
+            comprobadorDePasswordCNB2324 contrasenya = new comprobadorDePasswordCNB2324();
+
+            int ResultadoEsperado = contrasenya.Test(PasswordIntroducido);
+
+            Assert.AreEqual(PasswordEsperado, ResultadoEsperado);
+        }
+
+        public void validarContrasenya3()
+        {
+            int PasswordEsperado = 4;
+            string PasswordIntroducido = "C0ntr@s3ñ@S3gur@";
+
+            comprobadorDePasswordCNB2324 contrasenya = new comprobadorDePasswordCNB2324();
+
+            int ResultadoEsperado = contrasenya.Test(PasswordIntroducido);
+
+            Assert.AreEqual(PasswordEsperado, ResultadoEsperado);
+        }
+
+    }
+
+}
+
