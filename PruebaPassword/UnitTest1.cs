0a4acedda929f36bd1b43021ec37f85e66935579
BLANCA ARIAS <blaarisae@alu.edu.gva.es>
2024-03-12 18:37:07 +0100

    2 prueba

--- PruebaPassword/UnitTest1.cs
+++ PruebaPassword/UnitTest1.cs
@@ -15,5 +15,15 @@ namespace PruebaPassword
             comprobadorDePassword miPrueba = new comprobadorDePassword();
             Assert.AreEqual(-1, miPrueba.test(password));
         }
+
+        [TestMethod]
+        public void PasswordNoValida1()
+        {
+            string password = "abc";
+            comprobadorDePassword miPrueba = new comprobadorDePassword();
+            Assert.AreEqual(0, miPrueba.test(password));
+        }
     }
+
+
 }
